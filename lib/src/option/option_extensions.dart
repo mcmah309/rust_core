29aa9d162eb1331fd217831f90dd796e058e8728
mcmah309 <dillonmcmahon79@gmail.com>
2023-12-01 13:38:47 -0500

    feat: To option extension

---
+++ lib/src/option/option_extensions.dart
@@ -0,0 +1,21 @@
+import '../../rust_core.dart';
+
+
+extension ToOption1<T extends Object> on T? {
+
+  /// Returns [None] if this is null and Some(this) if this not null.
+  Option<T> toOption(){
+    if(this == null){
+      return const None();
+    }
+    return Some(this!);
+  }
+}
+
+extension ToOption2<T extends Object> on T {
+
+  /// Returns [None] if this is null and Some(this) if this not null.
+  Option<T> toOption(){
+    return Some(this);
+  }
+}
