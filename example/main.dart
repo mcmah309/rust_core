84d4b1fd7dcbb1479b76425bd309e647f66aabe2
mcmah309 <dillonmcmahon79@gmail.com>
2023-12-02 19:24:52 -0500

    doc: Add example

---
+++ example/main.dart
@@ -0,0 +1,25 @@
+import 'package:rust_core/core.dart'; // Or import 'package:rust_core/<LIBRARY_NAME>.dart';
+
+void main(){
+  usingTheEarlyReturnKey();
+  usingRegularPatternMatching();
+}
+
+Result<int,String> usingTheEarlyReturnKey() => Result.$(($){
+  double x = willAlwaysReturnErr()[$];
+  return Ok(1.toInt());
+});
+
+Result<int,String> usingRegularPatternMatching(){
+  double x;
+  switch(willAlwaysReturnErr()){
+    case Err(:final err):
+      return Err(err);
+    case Ok(:final ok):
+      x = ok;
+  }
+  return Ok(x.toInt());
+}
+
+Result<double,String> willAlwaysReturnErr() => Err("error");
+
