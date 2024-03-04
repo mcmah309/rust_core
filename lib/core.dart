70d12736f5dba9d5a3c65caf96426348f1907bd4
mcmah309 <dillonmcmahon79@gmail.com>
2024-03-04 02:51:26 +0000

    chore: Add core library

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+library core;
+
+export "array.dart";
+export "cell.dart";
+export "iter.dart";
+export "option.dart";
+export "panic.dart";
+export "result.dart";
+export "slice.dart";
+export "typedefs.dart";
