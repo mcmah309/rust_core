8b0b0b3f2eb7e54f46dda888e5723152a71b7694
mcmah309 <dillonmcmahon79@gmail.com>
2024-03-10 04:53:05 +0000

    fix: Flatmap fix

--- lib/src/iter/flat_map.dart
+++ lib/src/iter/flat_map.dart
@@ -1,21 +1,21 @@
 part of 'iterator.dart';
 
-
+/// Maps each element of the original iterator to an iterator, and then flattens the result into a single iterator.
 final class FlatMap<S, T> extends RIterator<T> {
   final Iterator<S> _iterator;
   final Iterator<T> Function(S) _f;
-  late Iterator<T> _currentExpansion;
+  Iterator<T>? _currentExpansion;
 
   FlatMap(this._iterator, this._f): super.late(){
     _wIterator = this;
   }
 
   @override
-  T get current => _currentExpansion.current;
+  T get current => _currentExpansion!.current;
 
   @override
   bool moveNext() {
-    while (_currentExpansion.moveNext()) {
+    while (_currentExpansion != null && _currentExpansion!.moveNext()) {
       return true;
     }
     if (_iterator.moveNext()) {
