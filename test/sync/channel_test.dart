13d5ce6441fc3bcfd3441b3e6fdacc8fd9b0de62
mcmah309 <dillonmcmahon79@gmail.com>
2024-06-30 21:50:26 +0000

    feat: Fixed list sized T codec

--- test/sync/channel_test.dart
+++ test/sync/channel_test.dart
@@ -2,158 +2,160 @@ import 'package:rust_core/sync.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("Single sender single receiver", () async {
-    List<int> results1 = [];
-    List<int> results2 = [];
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    tx.send(2);
-    tx.send(3);
-    tx.send(4);
-    tx.send(5);
-    await Future.delayed(Duration(milliseconds: 100));
-    for (int i = 0; i < 2; i++) {
+  group("channel", () {
+    test("Single sender single receiver", () async {
+      List<int> results1 = [];
+      List<int> results2 = [];
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      tx.send(2);
+      tx.send(3);
+      tx.send(4);
+      tx.send(5);
+      await Future.delayed(Duration(milliseconds: 100));
+      for (int i = 0; i < 2; i++) {
+        results1.add((await rx.recv()).unwrap());
+        await Future.delayed(Duration(milliseconds: 100));
+        results2.add((await rx.recv()).unwrap());
+      }
+      await Future.delayed(Duration(milliseconds: 100));
+      tx.send(6);
+      await Future.delayed(Duration(milliseconds: 100));
       results1.add((await rx.recv()).unwrap());
       await Future.delayed(Duration(milliseconds: 100));
       results2.add((await rx.recv()).unwrap());
-    }
-    await Future.delayed(Duration(milliseconds: 100));
-    tx.send(6);
-    await Future.delayed(Duration(milliseconds: 100));
-    results1.add((await rx.recv()).unwrap());
-    await Future.delayed(Duration(milliseconds: 100));
-    results2.add((await rx.recv()).unwrap());
-    await Future.delayed(Duration(milliseconds: 100));
-    tx.send(7);
-    await Future.delayed(Duration(milliseconds: 100));
-    results1.add((await rx.recv()).unwrap());
-
-    expect(results1, [1, 3, 5, 7]);
-    expect(results2, [2, 4, 6]);
-  });
-
-  test("Single sender multiple receiver", () async {
-    List<int> results1 = [];
-    List<int> results2 = [];
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    tx.send(2);
-    tx.send(3);
-    tx.send(4);
-    await Future.delayed(Duration(milliseconds: 100));
-    for (int i = 0; i < 2; i++) {
-      final r1 = rx.recv();
-      final r2 = rx.recv();
-      final rs = await Future.wait([r1, r2]);
-      results1.add(rs[0].unwrap());
       await Future.delayed(Duration(milliseconds: 100));
-      results2.add(rs[1].unwrap());
-    }
-    tx.send(5);
-    block() async {
-      final r1 = rx.recv();
-      final r2 = rx.recv();
-      final rs = await Future.wait([r1, r2]);
-      results1.add(rs[0].unwrap());
+      tx.send(7);
       await Future.delayed(Duration(milliseconds: 100));
-      results2.add(rs[1].unwrap());
-      return;
-    }
+      results1.add((await rx.recv()).unwrap());
 
-    final b = block();
-    await Future.delayed(Duration(milliseconds: 100));
-    tx.send(6);
-    await Future.delayed(Duration(milliseconds: 100));
-    await b;
+      expect(results1, [1, 3, 5, 7]);
+      expect(results2, [2, 4, 6]);
+    });
 
-    expect(results1, [1, 3, 5]);
-    expect(results2, [2, 4, 6]);
-  });
-
-  test("Receiver with timeout", () async {
-    List<int> results = [];
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    await Future.delayed(Duration(milliseconds: 50));
-    final result = await rx.recvTimeout(Duration(milliseconds: 100));
-    results.add(result.unwrap());
-
-    final timeoutResult = await rx.recvTimeout(Duration(milliseconds: 50));
-    expect(timeoutResult.isErr(), true);
-    expect(timeoutResult.unwrapErr(), isA<TimeoutError>());
-
-    expect(results, [1]);
-  });
-
-  test("Receiver with error handling", () async {
-    List<int> results = [];
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    tx.send(2);
-    (tx as LocalSender<int>).sink.addError(Exception("Test error"));
-    tx.send(3);
-    await Future.delayed(Duration(milliseconds: 100));
-    bool foundError = false;
-    for (int i = 0; i < 4; i++) {
-      final result = await rx.recv();
-      if (result.isOk()) {
-        results.add(result.unwrap());
-      } else {
-        final err = result.unwrapErr();
-        foundError = true;
-        expect(err, isA<OtherError>());
-        expect(i, 2);
+    test("Single sender multiple receiver", () async {
+      List<int> results1 = [];
+      List<int> results2 = [];
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      tx.send(2);
+      tx.send(3);
+      tx.send(4);
+      await Future.delayed(Duration(milliseconds: 100));
+      for (int i = 0; i < 2; i++) {
+        final r1 = rx.recv();
+        final r2 = rx.recv();
+        final rs = await Future.wait([r1, r2]);
+        results1.add(rs[0].unwrap());
+        await Future.delayed(Duration(milliseconds: 100));
+        results2.add(rs[1].unwrap());
       }
-    }
-    expect(foundError, true);
-    expect(results, [1, 2, 3]);
-  });
-
-  test("Receiver iter method", () async {
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    tx.send(2);
-    tx.send(3);
-    tx.send(4);
-    await Future.delayed(Duration(milliseconds: 100));
-
-    final iterator = rx.iter();
-    List<int> results = [];
-    for (final value in iterator) {
-      results.add(value);
-    }
-    expect(results, [1, 2, 3, 4]);
-    tx.send(5);
-    final value = await rx.recv();
-    expect(value.unwrap(), 5);
-  });
-
-  test("Receiver stream method", () async {
-    final (tx, rx) = channel<int>();
-    tx.send(1);
-    tx.send(2);
-    tx.send(3);
-    await Future.delayed(Duration(milliseconds: 100));
-    tx.send(4);
-    () async {
-      await Future.delayed(Duration(milliseconds: 1000));
-      (tx as LocalSender<int>).sink.close();
-    }();
-
-    List<int> results = [];
-    await for (final value in rx.stream()) {
-      results.add(value);
-    }
-    expect(results, [1, 2, 3, 4]);
-    bool threw = false;
-    try {
       tx.send(5);
-    } catch (e) {
-      threw = true;
-      expect(e, isA<StateError>());
-    }
-    expect(threw, true);
-    final value = await rx.recv();
-    expect(value.unwrapErr(), DisconnectedError());
+      block() async {
+        final r1 = rx.recv();
+        final r2 = rx.recv();
+        final rs = await Future.wait([r1, r2]);
+        results1.add(rs[0].unwrap());
+        await Future.delayed(Duration(milliseconds: 100));
+        results2.add(rs[1].unwrap());
+        return;
+      }
+
+      final b = block();
+      await Future.delayed(Duration(milliseconds: 100));
+      tx.send(6);
+      await Future.delayed(Duration(milliseconds: 100));
+      await b;
+
+      expect(results1, [1, 3, 5]);
+      expect(results2, [2, 4, 6]);
+    });
+
+    test("Receiver with timeout", () async {
+      List<int> results = [];
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      await Future.delayed(Duration(milliseconds: 50));
+      final result = await rx.recvTimeout(Duration(milliseconds: 100));
+      results.add(result.unwrap());
+
+      final timeoutResult = await rx.recvTimeout(Duration(milliseconds: 50));
+      expect(timeoutResult.isErr(), true);
+      expect(timeoutResult.unwrapErr(), isA<TimeoutError>());
+
+      expect(results, [1]);
+    });
+
+    test("Receiver with error handling", () async {
+      List<int> results = [];
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      tx.send(2);
+      (tx as LocalSender<int>).sink.addError(Exception("Test error"));
+      tx.send(3);
+      await Future.delayed(Duration(milliseconds: 100));
+      bool foundError = false;
+      for (int i = 0; i < 4; i++) {
+        final result = await rx.recv();
+        if (result.isOk()) {
+          results.add(result.unwrap());
+        } else {
+          final err = result.unwrapErr();
+          foundError = true;
+          expect(err, isA<OtherError>());
+          expect(i, 2);
+        }
+      }
+      expect(foundError, true);
+      expect(results, [1, 2, 3]);
+    });
+
+    test("Receiver iter method", () async {
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      tx.send(2);
+      tx.send(3);
+      tx.send(4);
+      await Future.delayed(Duration(milliseconds: 100));
+
+      final iterator = rx.iter();
+      List<int> results = [];
+      for (final value in iterator) {
+        results.add(value);
+      }
+      expect(results, [1, 2, 3, 4]);
+      tx.send(5);
+      final value = await rx.recv();
+      expect(value.unwrap(), 5);
+    });
+
+    test("Receiver stream method", () async {
+      final (tx, rx) = channel<int>();
+      tx.send(1);
+      tx.send(2);
+      tx.send(3);
+      await Future.delayed(Duration(milliseconds: 100));
+      tx.send(4);
+      () async {
+        await Future.delayed(Duration(milliseconds: 1000));
+        (tx as LocalSender<int>).sink.close();
+      }();
+
+      List<int> results = [];
+      await for (final value in rx.stream()) {
+        results.add(value);
+      }
+      expect(results, [1, 2, 3, 4]);
+      bool threw = false;
+      try {
+        tx.send(5);
+      } catch (e) {
+        threw = true;
+        expect(e, isA<StateError>());
+      }
+      expect(threw, true);
+      final value = await rx.recv();
+      expect(value.unwrapErr(), DisconnectedError());
+    });
   });
 }
--- test/sync/isolate_channel_test.dart
+++ test/sync/isolate_channel_test.dart
@@ -3,43 +3,84 @@ import 'package:rust_core/sync.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  test("Simple isolate channel", () async {
-    final (tx1, rx1) = await isolateChannel<String>(const StringCodec(), (tx2, rx2) async {
-      assert((await rx2.recv()).unwrap() == "hello");
-      tx2.send("hi");
+  group("isolateChannel", () {
+    test("Simple isolate channel", () async {
+      final (tx1, rx1) = await isolateChannel<String>(const StringCodec(), (tx2, rx2) async {
+        assert((await rx2.recv()).unwrap() == "hello");
+        tx2.send("hi");
+      });
+      tx1.send("hello");
+      expect((await rx1.recv()).unwrap(), "hi");
     });
-    tx1.send("hello");
-    expect((await rx1.recv()).unwrap(), "hi");
+
+    test("Bi-directional send and receive", () async {
+      final (tx1, rx1) = await isolateChannel<int>(const IntCodec(), (tx2, rx2) async {
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send((await rx2.recv()).unwrap() * 10);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send((await rx2.recv()).unwrap() * 10);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send(6);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send((await rx2.recv()).unwrap() * 10);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send((await rx2.recv()).unwrap() * 10);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send(7);
+        await Future.delayed(Duration(milliseconds: 100));
+        tx2.send((await rx2.recv()).unwrap() * 10);
+      });
+      tx1.send(1);
+      expect(await rx1.recv().unwrap(), 10);
+      tx1.send(2);
+      expect(await rx1.recv().unwrap(), 20);
+      tx1.send(3);
+      expect(await rx1.recv().unwrap(), 6);
+      tx1.send(4);
+      expect(await rx1.recv().unwrap(), 30);
+      tx1.send(5);
+      expect(await rx1.recv().unwrap(), 40);
+      expect(await rx1.recv().unwrap(), 7);
+      expect(await rx1.recv().unwrap(), 50);
+    });
   });
 
-  test("Bi-directional send and receive", () async {
-    final (tx1, rx1) = await isolateChannel<int>(const IntCodec(), (tx2, rx2) async {
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send((await rx2.recv()).unwrap() * 10);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send((await rx2.recv()).unwrap() * 10);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send(6);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send((await rx2.recv()).unwrap() * 10);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send((await rx2.recv()).unwrap() * 10);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send(7);
-      await Future.delayed(Duration(milliseconds: 100));
-      tx2.send((await rx2.recv()).unwrap() * 10);
+  group("Codec", () {
+    test("StringCodec", (){
+      final codec = const StringCodec();
+      expect(codec.decode(codec.encode("Hello")), "Hello");
     });
-    tx1.send(1);
-    expect(await rx1.recv().unwrap(), 10);
-    tx1.send(2);
-    expect(await rx1.recv().unwrap(), 20);
-    tx1.send(3);
-    expect(await rx1.recv().unwrap(), 6);
-    tx1.send(4);
-    expect(await rx1.recv().unwrap(), 30);
-    tx1.send(5);
-    expect(await rx1.recv().unwrap(), 40);
-    expect(await rx1.recv().unwrap(), 7);
-    expect(await rx1.recv().unwrap(), 50);
+
+    test("IntCodec", (){
+      final codec = const IntCodec();
+      expect(codec.decode(codec.encode(1)), 1);
+    });
+
+    test("DoubleCodec", (){
+      final codec = const DoubleCodec();
+      expect(codec.decode(codec.encode(1.0)), 1.0);
+    });
+
+
+    test("BooleanCodec", (){
+      final codec = const BooleanCodec();
+      expect(codec.decode(codec.encode(true)), true);
+    });
+
+    test("IntListCodec", (){
+      final codec = const IntListCodec();
+      expect(codec.decode(codec.encode([1, 2, 3])), [1, 2, 3]);
+    });
+
+    test("DoubleListCodec", (){
+      final codec = const DoubleListCodec();
+      expect(codec.decode(codec.encode([1.0, 2.0, 3.0])), [1.0, 2.0, 3.0]);
+    });
+
+    test("BooleanListCodec", (){
+      final codec = const BooleanListCodec();
+      expect(codec.decode(codec.encode([true, false])), [true, false]);
+    });
+
   });
 }
