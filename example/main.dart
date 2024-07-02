401a7295d55007caaf076fe71cef4148b253b15e
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-02 19:38:16 +0000

    chore: Update example

--- example/main.dart
+++ example/main.dart
@@ -9,7 +9,7 @@ void main() {
   iteratorExample();
   sliceExample();
 
-  /// Visit the library links to see more!
+  /// Visit the book to see more!
 }
 
 Result<int, String> usingTheEarlyReturnKeyExample() => Result(($) {
@@ -40,29 +40,21 @@ void iteratorExample() {
       collect.add(e);
     }
   }
-  //expect(collect, [4, 16]);
   Option<int> next = rIterator.next();
-  //expect(next, Some(6));
   collect.add(next.unwrap());
   next = rIterator.next();
   collect.add(next.unwrap());
-  //expect(next, Some(7));
   while (rIterator.moveNext()) {
     collect.add(rIterator.current * rIterator.current);
   }
-  //expect(collect, [4, 16, 6, 7, 64, 81]);
-  //expect(rIterator,[]);
 }
 
 void sliceExample() {
   var list = [1, 2, 3, 4, 5];
   var slice = Slice(list, 1, 4);
-  //expect(slice, [2, 3, 4]);
   var taken = slice.takeLast();
-  //expect(taken, 4);
-  //expect(slice, [2, 3]);
   slice[1] = 10;
-  //expect(list, [1, 2, 10, 4, 5]);
+  assert(list[2] == 10);
 }
 
 Result<double, String> willAlwaysReturnErr() => Err("error");
