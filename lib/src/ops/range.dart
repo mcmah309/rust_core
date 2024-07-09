874f505eb62ba47cc6bae8506946cc17a7d5a29f
mcmah309 <dillonmcmahon79@gmail.com>
2024-07-09 16:23:04 +0000

    feat: Change range to python range

--- lib/src/ops/range.dart
+++ lib/src/ops/range.dart
@@ -1,50 +1,61 @@
-
 import 'package:rust_core/iter.dart';
 import 'package:rust_core/panic.dart';
 
-/// An iterator over the range [start..end), where start >= end or start <= end.
+/// A generator over the range by step.
+/// If [end] is not provided, range will be [0..startOrEnd), where [startOrEnd] can be positive or negative.
+/// If [step] is not provided, step will be `-1` if `0 > startOrEnd` and `1` if `0 < startOrEnd`.
+/// For reference, it works the same as the python `range` function.
+/// ```dart
+/// range(end); // == range(0,end);
+/// range(start, end);
+/// range(start, end, step);
+/// ```
+/// 
+// Dev Note: inlined for parameter optimization
 @pragma("vm:prefer-inline")
-RIterator<int> range(int start, int end, [int? step]) {
-  if (step == null) {
-    return RIterator.fromIterable(_range(start, end));
+Iterable<int> range(int startOrEnd, [int? end, int? step]) sync* {
+  assert(!(end == null && step != null),
+      "'step' cannot be given if 'end' is null. Step will be ignored in release mode.");
+  if (step == 0) {
+    panic("'step' cannot be zero");
   }
-  return RIterator.fromIterable(_rangeWithStep(start, end, step));
-}
-
-@pragma("vm:prefer-inline")
-Iterable<int> _range(int start, int end) sync* {
-  if (start < end) {
-    final step = 1;
-    do {
-      yield start;
-      start += step;
-    } while (start < end);
-  } else {
-    final step = -1;
-    while (end < start) {
-      yield start;
-      start += step;
+  if (end == null) {
+    int current = 0;
+    if (startOrEnd > 0) {
+      do {
+        yield current;
+        current++;
+      } while (current < startOrEnd);
+    } else {
+      while (current > startOrEnd) {
+        yield current;
+        current--;
+      }
     }
+    return;
   }
-}
-
-@pragma("vm:prefer-inline")
-Iterable<int> _rangeWithStep(int start, int end, int step) sync* {
-  if (start < end) {
-    if (step <= 0) {
+  if (startOrEnd == end) {
+    return;
+  }
+  if (startOrEnd < end) {
+    if (step == null) {
+      step = 1;
+    } else if (step < 0) {
       panic("'step' needs to be positive when start < end");
     }
     do {
-      yield start;
-      start += step;
-    } while (start < end);
+      yield startOrEnd;
+      startOrEnd += step;
+    } while (startOrEnd < end);
   } else {
-    if (step >= 0 && end < start) {
+    if (step == null) {
+      step = -1;
+    } else if (step > 0) {
       panic("'step' needs to be negative when start > end");
     }
-    while (end < start) {
-      yield start;
-      start += step;
+    while (startOrEnd > end) {
+      yield startOrEnd;
+      startOrEnd += step;
     }
   }
 }
--- test/ops/range_test.dart
+++ test/ops/range_test.dart
@@ -5,23 +5,20 @@ import 'package:test/test.dart';
 void main() {
   group('range', () {
     test('Range ascending', () {
-      expect((5, 10).range().toList(), equals([5, 6, 7, 8, 9]));
+      expect(range(5, 10).toList(), equals([5, 6, 7, 8, 9]));
       expect(range(5, 10).toList(), equals([5, 6, 7, 8, 9]));
     });
 
     test('Range descending', () {
-      expect((10, 5).range().toList(), equals([10, 9, 8, 7, 6]));
       expect(range(10, 5).toList(), equals([10, 9, 8, 7, 6]));
     });
 
     test('Range equal', () {
-      expect((5, 5).range().toList(), equals([]));
       expect(range(5, 5).toList(), equals([]));
     });
 
     test("Range ascending with step", () {
       expect(range(5, 10, 1).toList(), equals([5, 6, 7, 8, 9]));
-      expect((5, 10, 1).range().toList(), equals([5, 6, 7, 8, 9]));
       expect(range(5, 10, 2).toList(), equals([5, 7, 9]));
       expect(() => range(5, 10, 0).toList(), throwsA(isA<Panic>()));
       expect(() => range(5, 10, -1).toList(), throwsA(isA<Panic>()));
@@ -30,7 +27,6 @@ void main() {
 
     test("Range descending with step", () {
       expect(range(10, 5, -1).toList(), equals([10, 9, 8, 7, 6]));
-      expect((10, 5, -2).range().toList(), equals([10, 8, 6]));
       expect(() => range(10, 5, 0).toList(), throwsA(isA<Panic>()));
       expect(() => range(10, 5, 1).toList(), throwsA(isA<Panic>()));
       expect(range(10, 5, -10).toList(), equals([10]));
@@ -38,24 +34,8 @@ void main() {
 
     test("Range equal with step", () {
       expect(range(5, 5, 1).toList(), equals([]));
-      expect((5, 5, 1).range().toList(), equals([]));
-      expect(range(5, 5, 0).toList(), equals([]));
+      expect(() => range(5, 5, 0).toList(), throwsA(isA<Panic>()));
       expect(range(5, 5, -1).toList(), equals([]));
-    });
-  });
-
-  group('Range extensions', () {
-    test("stepBy", () {
-      List<int> collection = [];
-      for (final x in (5, 10).stepBy(2)) {
-        collection.add(x);
-      }
-      expect(collection, equals([5, 7, 9]));
-
-      collection.clear();
-      for (final x in (5, 10, 2).range()) {
-        collection.add(x);
-      }
     });
   });
 }
